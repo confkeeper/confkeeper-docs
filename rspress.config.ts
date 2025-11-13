9e5a586ed240d7c66c1f9dd2dfd5ad50de8e979e
buyfakett <buyfakett@vip.qq.com>
2025-11-13 09:56:58 +0000

    优化文档

--- rspress.config.ts
+++ rspress.config.ts
@@ -8,15 +8,27 @@ export default defineConfig({
   title: 'confkeeper文档',
   icon: '/confkeeper.png',
   outDir: 'dist',
+  search: {
+    codeBlocks: true,
+  },
+  markdown: {
+    showLineNumbers: true,
+  },
+  route: {
+    cleanUrls: true,
+  },
+  ssg: {
+    experimentalWorker: true,
+  },
   themeConfig: {
+    fallbackHeadingTitle: false,
+    prevPageText: '上一页',
+    nextPageText: '下一页',
     socialLinks: [
       { icon: 'github', mode: 'link', content: 'https://github.com/confkeeper/confkeeper-docs' },
     ],
     footer: {
       message: 'Apache-2.0 license<br />Copyright © 2025 <a href="https://github.com/buyfakett" target="_blank" style="color: skyblue">buyfakett</a>',
     }
-  },
-  markdown: {
-    showLineNumbers: true,
   }
 });
